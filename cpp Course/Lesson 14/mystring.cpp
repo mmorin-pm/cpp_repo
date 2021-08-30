f6cd2ab2e4b38b291db9920565232f0a369d12d5
mmorin-pm <morin.marcel@mac.com>
2021-08-29 19:07:52 -0500

    operator overloading examples

---
+++ cpp Course/Lesson 14/mystring.cpp
@@ -0,0 +1,72 @@
+#include <cstring>
+#include <iostream>
+#include "mystring.h"
+
+// No-args constructor
+Mystring::Mystring() 
+    : str{nullptr} {
+    str = new char[1];
+    *str = '\0';
+}
+
+// Overloaded constructor
+Mystring::Mystring(const char *s) 
+    : str {nullptr} {
+        if (s==nullptr) {
+            str = new char[1];
+            *str = '\0';
+        } else {
+            str = new char[std::strlen(s)+1];
+            std::strcpy(str, s);
+        }
+}
+
+// Move constructor
+Mystring::Mystring(Mystring &&source)
+    : str(source.str) {
+        source.str = nullptr;
+        std::cout << "Move constructor used" << std::endl;
+}
+
+// Copy constructor
+Mystring::Mystring(const Mystring &source) 
+    : str{nullptr} {
+        str = new char[std::strlen(source.str) + 1];
+        std::strcpy(str, source.str);
+}
+
+// Destructor
+Mystring::~Mystring() {
+    delete [] str;
+}
+
+// Copy assignment
+Mystring &Mystring::operator=(const Mystring &rhs) {
+    std::cout << "Copy assignment" << std::endl;
+    if (this == &rhs)
+        return *this;
+    delete [] this->str; // delete whatever lhs/this is pointing to as we are going to assign it
+    str = new char[std::strlen(rhs.str) + 1];
+    std::strcpy(this->str, rhs.str);
+    return *this;
+}
+
+// Move assignment
+Mystring &Mystring::operator=(Mystring &&rhs) {
+    std::cout << "Move assignment" << std::endl;
+    if (this == &rhs)
+        return *this;   
+    delete [] this->str;
+    str = rhs.str;
+    rhs.str = nullptr;
+    return *this;
+};
+
+// Display method
+void Mystring::display() const {
+    std::cout << str << " : " << get_length() << std::endl;
+}
+
+// getters
+ int Mystring::get_length() const { return strlen(str); }
+ const char *Mystring::get_str() const { return str; }
